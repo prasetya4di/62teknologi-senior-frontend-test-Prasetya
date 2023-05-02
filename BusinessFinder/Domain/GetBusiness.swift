7b7bce575c85d092ea1ccc7bc2f83195ddad8218
Prasetya Adi Arfanto <prasetyaarfanto@gmail.com>
2023-05-03 06:56:07 +0700

    Add get business use case

---
+++ BusinessFinder/Domain/GetBusiness.swift
@@ -0,0 +1,30 @@
+//
+//  GetBusiness.swift
+//  BusinessFinder
+//
+//  Created by Prasetya on 02/05/23.
+//
+
+import Foundation
+import Combine
+
+protocol GetBusiness {
+    func call() async throws -> [Business]
+}
+
+class GetBusinessImpl: GetBusiness {
+    private let businessRepository: BusinessRepository
+    
+    init(businessRepository: BusinessRepository) {
+        self.businessRepository = businessRepository
+    }
+    
+    func call() async throws -> [Business] {
+        try await businessRepository
+            .getBusiness(
+                location: Locale.current.language.region?.identifier ?? "ID")
+            .businesses.map { response in
+                Business(response: response)
+            }
+    }
+}
