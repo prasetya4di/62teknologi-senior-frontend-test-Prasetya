0d2082e29de3bb23dbcbfaeda977b9a2e8ab36a6
Prasetya Adi Arfanto <prasetyaarfanto@gmail.com>
2023-05-02 14:53:50 +0700

    Initial Commit

---
+++ BusinessFinder/BusinessFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessFinderApp.swift
+//  BusinessFinder
+//
+//  Created by Prasetya on 02/05/23.
+//
+
+import SwiftUI
+
+@main
+struct BusinessFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
