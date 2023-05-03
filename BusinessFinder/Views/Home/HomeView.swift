d960dbcba22f9a280962a09731afa23def7ead47
Prasetya Adi Arfanto <prasetyaarfanto@gmail.com>
2023-05-03 07:41:58 +0700

    Update home view

--- BusinessFinder/Views/Home/HomeView.swift
+++ BusinessFinder/Views/Home/HomeView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Text("Business")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            BusinessList()
+        }
+        .padding()
     }
 }
 
