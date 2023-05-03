0e224b18f6ed90dd05decda6b3c16a1614bc4659
Prasetya Adi Arfanto <prasetyaarfanto@gmail.com>
2023-05-03 19:51:57 +0700

    Add country option struct

---
+++ BusinessFinder/Data/Model/Entity/CountryOption.swift
@@ -0,0 +1,13 @@
+//
+//  CountryOption.swift
+//  BusinessFinder
+//
+//  Created by Prasetya on 03/05/23.
+//
+
+import Foundation
+
+struct CountryOption: Hashable {
+    let label: String
+    let value: String
+}
