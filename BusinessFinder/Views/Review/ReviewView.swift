b17c3da08a16c1cb97f17a11180e009c60a4ac0c
Prasetya Adi Arfanto <prasetyaarfanto@gmail.com>
2023-05-03 14:42:12 +0700

    Use view model to get list review

--- BusinessFinder/Views/Review/ReviewView.swift
+++ BusinessFinder/Views/Review/ReviewView.swift
@@ -8,7 +8,8 @@
 import SwiftUI
 
 struct ReviewView: View {
-    let review: [Review]
+    let id: String
+    @EnvironmentObject private var viewModel: ReviewViewModel
     
     var body: some View {
         VStack(alignment: .leading) {
@@ -18,29 +19,10 @@ struct ReviewView: View {
             
             Divider()
             
-            ReviewList(review: review)
+            ReviewList(review: viewModel.viewState.reviews)
         }
-    }
-}
-
-struct ReviewView_Previews: PreviewProvider {
-    static var previews: some View {
-        let review = Review(
-            id: "id",
-            url: "https://www.yelp.com/biz/barbacoa-grill-boise?adjust_creative=DSj6I8qbyHf-Zm2fGExuug&hrid=MD_RWUiEj4scIW0C1X930g&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_reviews&utm_source=DSj6I8qbyHf-Zm2fGExuug",
-            text: "This restaurant is on FIRE.  From the moment you walk in the door- you're the ultimate hipster and the staff give you the love like you are.  \n\nThe food is...",
-            rating: 5,
-            timeCreated: "2023-03-31 10:46:13",
-            user: User(
-                id: "JmWLTphef61ujj7LvrQKTw",
-                profileURL: "https://www.yelp.com/user_details?userid=JmWLTphef61ujj7LvrQKTw",
-                imageURL: "https://s3-media2.fl.yelpcdn.com/photo/0eBdAsNYSt-Idz50tW0ABA/o.jpg",
-                name: "Paul L."))
-        
-        ReviewView(review: [
-            review,
-            review,
-            review
-        ])
+        .onAppear {
+            viewModel.fetchReviews(id: id)
+        }
     }
 }
