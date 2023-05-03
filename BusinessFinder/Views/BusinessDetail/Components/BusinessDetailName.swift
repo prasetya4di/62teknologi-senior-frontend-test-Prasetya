f7e5dcbee86febe228855d041ce111f0f40498de
Prasetya Adi Arfanto <prasetyaarfanto@gmail.com>
2023-05-03 11:36:34 +0700

    Add business detail name

---
+++ BusinessFinder/Views/BusinessDetail/Components/BusinessDetailName.swift
@@ -0,0 +1,24 @@
+//
+//  BusinessDetailName.swift
+//  BusinessFinder
+//
+//  Created by Prasetya on 03/05/23.
+//
+
+import SwiftUI
+
+struct BusinessDetailName: View {
+    let name: String
+    
+    var body: some View {
+        Text(name)
+            .font(.title)
+            .fontWeight(.bold)
+    }
+}
+
+struct BusinessDetailName_Previews: PreviewProvider {
+    static var previews: some View {
+        BusinessDetailName(name: "Barbacoa Grill")
+    }
+}
